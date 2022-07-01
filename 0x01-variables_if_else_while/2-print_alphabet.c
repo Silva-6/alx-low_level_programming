cd2f17225c1049ca603839132cf87f8dd7d91b5a
Silva-6 <silva6media@gmail.com>
2022-07-01 10:25:05 -0700

    Updated: Alphabets in lowercase, in uppercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints a-z to standard output
+ * Return: Exit status is zero means success
+ */
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Prints a-z to standard output
+ * Return: Exit status is zero means success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+		putchar(i);
+	for (i = 65; i <= 90; i++)
+		putchar(i);
+	putchar('\n');
+	
+	return (0);
+}
