d102e197be5966263a2ba1d5c1afb31c9ca24424
Silva-6 <silva6media@gmail.com>
2022-07-07 03:15:50 -0700

    All files for the project

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _islower - Check for lowercase ASCII characters
+ * i_salpha - Check for alphabet characters
  * @c: This is an ASCII character
  *
  * Return: 0 if not an alphabet, 1 if alphabet
  */
 int _isalpha(int c)
 {
-	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 		return (1);
 	return (0);
 }
