bc7aa59bf486dc4b79983240415c91ab7af78a37
Silva-6 <silva6media@gmail.com>
2022-07-07 02:10:08 -0700

    All files for the project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
 /**
- * _alphabet - Prints a-z in lowercase
+ * print_alphabet - Prints a-z in lowercase
  * Return: returns nothing/void
  */
-void _alphabet(void)
+void print_alphabet(void)
 {
 	char alphabet = 'a';
+
 	while (alphabet <= 'z')
 	{
 		_putchar(alphabet);
 		alphabet++;
 	}
+
 	_putchar('\n');
 }
