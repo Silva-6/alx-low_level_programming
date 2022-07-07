d509615a3e55e2d6c2561a5f9937b05fbb8862e3
Silva-6 <silva6media@gmail.com>
2022-07-07 06:35:49 -0700

    Project files 6-11 (mandatory)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * add - Sums up two numbers
+ * @num1: This is an integer
+ * @num2: This is an integer
+ *
+ * Return: returns the sum
+ */
+int add(int num1, int num2)
+	return (num1 + num2);
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Null, Void
+ */
+void times_table(void)
+{
+	int mm, nn, mul;
+	
+	for (mm = 0; mm <= 9; mm++)
+	{
+		for (nn = 0; nn <=9; nn++)
+		{
+			mul = mm * nn;
+			if (mul < 10)
+			{
+				_putchar(' ');
+				_putchar('0' + mul);
+			}
+			_putchar('0' + mul / 10);
+			_putchar('0' + mul % 10);
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
