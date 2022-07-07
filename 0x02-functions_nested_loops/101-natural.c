1f20c75804c3bf4fd9f9a8a37a3c6daf3d92c493
Silva-6 <silva6media@gmail.com>
2022-07-07 10:08:13 -0700

    All the files for the project (Advanced)

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Computes and prints the sum of all the multiples
+ * of 3 0r 5 below 1024
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 ==0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 fibanacci numbers
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	long int n1 = 1, result, n2= 2, nLast = 50;
+	int i;
+
+	printf("%lu, %lu, ", n1, n2);
+	for (i = 2; i < nLast; i++)
+	{
+		result = n1 + n2;
+		n1 = n2;
+		n2 = result;
+		printf("%lu", result);
+		if (i != nLast - 1)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
