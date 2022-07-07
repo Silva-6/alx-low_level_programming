40551fac1dded2096a77a8d8e6ccef450635067e
Silva-6 <silva6media@gmail.com>
2022-07-07 03:45:29 -0700

    All the files for this project

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - Checks for -ve, +ve or 0 integers
+ * @n: This is a real number
+ *
+ * Return: 0 if zero, 1 if > 0 and -1 if < 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);	
+	}
+	putchar('0')
+	return (0);
+}
---
+++ general.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void foo()
+{
+	printf("foo called");
+}
+
+int main()
+{
+	foo();
+	getchar();
+	return 0;
+}
