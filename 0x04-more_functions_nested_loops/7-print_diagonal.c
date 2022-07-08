c2eb68745774b1771cec3468977f927c0ea80fab
Silva-6 <silva6media@gmail.com>
2022-07-08 02:11:48 -0700

    Updated

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,8 +17,12 @@ void print_diagonal(int n)
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
-				_putchar(' ');
-			_putchar('\\');
+			{
+				if (i == j)
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
