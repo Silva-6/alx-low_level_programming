62eff9b4679ca486e643b1e10919fb4c5cb59508
Silva-6 <silva6media@gmail.com>
2022-07-07 19:17:59 -0700

    Some fixes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include "main.h"
+#define leapDaysTot 366
+#define daysTot 365
+#define janDaysTot 31
 
 /**
  * print_remainig_days - takes a date and prints how many days
@@ -12,7 +15,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))
+	if ((year % 100 != 0 || year % 400 == 0) && year % 4 == 0)
 	{
 		if (month > 2 && day >= 60)
 			day++;
