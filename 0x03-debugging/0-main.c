75cde6ff2744e7104fb42fe231eb061fa2f80832
Silva-6 <silva6media@gmail.com>
2022-07-07 18:51:33 -0700

    All the files for the project

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#inclue "main.h"
+
+/**
+ * main - Tests function that prints is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	retun (0);
+}
