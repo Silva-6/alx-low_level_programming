812bf60932babe261c54a141df1b223a03de5c88
Silva-6 <silva6media@gmail.com>
2022-07-14 11:21:33 -0700

    Updated

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (count == 0)
 	{
-		if (*(s1 + count2) == '\0' && *(s2 + count2) =='\0')
+		if (*(s1 + count2) == '\0' && *(s2 + count2) == '\0')
 			break;
 		count = *(s1 + count2) - *(s2 + count2);
 		count2++;
