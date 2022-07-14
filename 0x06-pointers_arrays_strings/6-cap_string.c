9bf7ec1e2b2e89027f00562d9a5ec166e39f0d12
Silva-6 <silva6media@gmail.com>
2022-07-14 11:48:07 -0700

    Updated

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (while (*(s + count) == sep_words[i]))
+			if (*(s + count) == sep_words[i])
 			{
 				if (*(s + (count + 1)) >= 97 && *(s = (count + 1)) <= 122)
 					*(s + (count + 1)) = *(s + (count + 1)) - 32;
