2143b7db43a855c40d0cbb037a587a819451eafd
Oluwaseun-D'Silva <106748698+Silva-6@users.noreply.github.com>
2022-07-15 05:45:27 +0100

    Update 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,16 +2,15 @@
 
 /**
  * rot13 - This implements the rot13 cipher algorithm
- * @s: string to be ciphered
+ * @s: This is the string to be ciphered
  *
  * Return: The ciphered string
  */
 char *rot13(char *s)
 {
 	int i, j;
-
 	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char cipher[] = "nopqrstuvwxyabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char cipher[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
@@ -24,5 +23,6 @@ char *rot13(char *s)
 			}
 		}
 	}
+
 	return (s);
 }
