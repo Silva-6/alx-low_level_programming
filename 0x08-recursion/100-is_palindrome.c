99ee64a6d0f78924a2a0cd1e98e8249e30ceefe7
Oluwaseun-D'Silva <106748698+Silva-6@users.noreply.github.com>
2022-07-21 03:49:58 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,25 +5,34 @@
  * @s: pointer to string params
  * Return: recursion
  */
+
 int _strlen_recursion(char *s)
 {
-	if (!s)
+	if (!*s)
+	{
 		return (0);
+	}
 	return (1 + _strlen_recursion(++s));
 }
 
 /**
  * p1 - palindrome
- * @s: POINTER TO STRING
+ * @s: pointer to string
  * @l: position
- * Return: boolean
+ * Return: boolena
  */
+
 int p1(char *s, int l)
 {
 	if (l < 1)
+	{
 		return (1);
-	if (*s == *(s + 1))
+	}
+
+	if (*s == *(s + l))
+	{
 		return (p1(s + 1, l - 2));
+	}
 	return (0);
 }
 
@@ -32,8 +41,10 @@ int p1(char *s, int l)
  * @s: pointer to string
  * Return: recursion
  */
+
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
+
 	return (p1(s, len - 1));
 }
