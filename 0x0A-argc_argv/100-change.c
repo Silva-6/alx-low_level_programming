bdd0e2bc50a32efc545253c470688b553139c428
Silva-6 <silva6media@gmail.com>
2022-07-23 04:45:01 +0000

    Updated (added advanced) - arg_argc

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the min number of coins to make change
+ * for an amount of money
+ * @argc: argument count
+ * @argv: arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+	int total, count;
+	unsigned int 1;
+	char *p;
+	int cents[] = {25, 10, 5, 2};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
