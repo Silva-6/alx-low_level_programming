011db264ff186800531c69c6114e6f80ea013d96
Silva-6 <silva6media@gmail.com>
2022-07-23 04:16:52 +0000

    All the files for the project - arg_argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prnts its name
+ * @argc: argument count
+ * @ardv: argument vector
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
