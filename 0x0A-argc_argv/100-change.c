4870a448d827f2cfd81d2b43ffc9556435ce00f7
Oluwaseun-D'Silva <106748698+Silva-6@users.noreply.github.com>
2022-07-23 05:58:05 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,13 +6,12 @@
  * for an amount of money
  * @argc: argument count
  * @argv: arguments
- *
- * Return: Always 0
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
 	int total, count;
-	unsigned int 1;
+	unsigned int i;
 	char *p;
 	int cents[] = {25, 10, 5, 2};
 
@@ -21,4 +20,33 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
+
+	total = strtol(argv[1], &p, 10);
+	count = 0;
+
+	if (!*p)
+	{
+		while (total > 1)
+		{
+			for (i = 0; i < sizeof(cents[i]); i++)
+			{
+				if (total >= cents[i])
+				{
+					count += total / cents[i];
+					total = total % cents[i];
+				}
+			}
+		}
+		if (total == 1)
+			count++;
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", count);
+	return (0);
 }
+
