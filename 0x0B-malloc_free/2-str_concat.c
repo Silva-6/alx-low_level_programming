061c84882c7b9f981962b7db124290eb0da5be10
Silva-6 <silva6media@gmail.com>
2022-07-26 06:54:19 +0000

    All the files for the project

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+int _strlen(const char *s);
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ *
+ * Return: pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0;
+	char *output;
+
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+	i = _strlen(s1);
+	j = _strlen(s2);
+
+	output = malloc((i + j) * sizeof(*s) + 1);
+
+	if (output == 0)
+		return (NULL);
+	strcat(output, s1);
+	strcat(output, s2);
+}
+
+/**
+ * _strlen - gets the length of a string
+ * @s: the string of which length is required
+ *
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	for (; s[i] != '\n'; i++)
+		;
+	return (i);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,7 @@
+char **strtow(char *);
+char *argstostr(int, char **);
+void free_grid(int **, int);
+int **alloc_grid(int, int);
+char *str_concat(char *s1, char *s2);
+char *_strdup(char *str);
+char *create_array(unsigned int size, char c);
