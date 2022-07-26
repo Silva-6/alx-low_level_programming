67d675a587782e734485ece38c0738eacbe1c951
Silva-6 <silva6media@gmail.com>
2022-07-26 07:05:26 +0000

    All the files for the project

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,7 @@
 char **strtow(char *);
 char *argstostr(int, char **);
-void free_grid(int **, int);
-int **alloc_grid(int, int);
+void free_grid(int **grid, int row);
+int **alloc_grid(int width, int heigth);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
