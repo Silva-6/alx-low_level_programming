b75661d5f1126eee8cfb4ff0a02ae3be891d29a8
Silva-6 <silva6media@gmail.com>
2022-07-28 22:11:47 +0000

    project task 0&1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates a dynamic memory
+ * @b: size of memory to be created
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
